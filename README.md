bbd1ef230e2e77510d00d0f1c11a66d5f47bd4cc
gabinpoa <99542800+gabinpoa@users.noreply.github.com>
2022-12-09 03:10:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Comunidade-Recome-ar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-r4d7h3)
---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <Image
+        src="https://drive.google.com/file/d/1QclxFu9vDoGfya_xUQLyNcMIAq37WKii/view?usp=sharing"
+        width={320}
+        height={280}
+      />
+    </div>
+  );
+}
